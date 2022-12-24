00e5ca70873c7f93b7093090771e30a0605d0791
amir mohammad <amirmohammad@gamil.com>
2022-12-24 23:33:54 +0330

    edit file app.py and create readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Change the Linux desktop background via a python script
+
+automatic download background image microsoft Bing web search engine and change my linux desktop background with image downloaded by Selenium framework in python :)
---
+++ app.py
@@ -0,0 +1,38 @@
+
+
+import time
+import subprocess
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+options = Options()
+options.add_experimental_option("detach", True)
+
+
+driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()),
+                          options = options)
+
+
+driver.get("https://www.bing.com/")
+driver.maximize_window()
+
+time.sleep(3)
+
+
+download_btn = driver.find_element("xpath", "//a[@class='downloadLink ']")
+driver.implicitly_wait(10)
+ActionChains(driver).move_to_element(download_btn).click(download_btn).perform()
+
+time.sleep(3)
+driver.quit()
+
+
+subprocess.run('mv /home/amir/Downloads/BingWallpaper.jpg /tmp', shell = True)
+subprocess.run("gsettings set org.gnome.desktop.background picture-uri-dark 'file:///tmp/BingWallpaper.jpg'", shell = True)
+
+
+
